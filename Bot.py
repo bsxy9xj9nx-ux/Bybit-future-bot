1877eb269535a3903ded627b284552f081029e34
bsxy9xj9nx-ux <bsxy9xj9nx@privaterelay.appleid.com>
2026-04-26 16:23:29 +0300

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,7 @@
+from telegram import Bot
+from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
+
+bot = Bot(token=TELEGRAM_TOKEN)
+
+async def send_message(text):
+    await bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=text)
